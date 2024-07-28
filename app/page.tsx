967275063af407b6e7fd23fb8bd0abc6d24540db
Komronbek5704 <qudratovkomron2004@gmail.com>
2024-07-29 01:57:24 +0500

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function AboutLayout(){
+    return(
+        <div>
+           <h1>About</h1>
+           <p>This is About Page</p>
+        </div>
+    )
+    
+}
